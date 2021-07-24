f574d64dc46923a303873e106aeb1891ea0d6253
Muzain187 <muzain.ashraf@gmail.com>
2021-07-24 15:00:33 +0530

    checksequence.cpp file is added

---
+++ checksequence.cpp
@@ -0,0 +1,24 @@
+bool checksequenece(char large[] , char*small) {
+    if(small[0] == '\0')
+        return true;
+    
+    if(large[0] == '\0'){
+    	return false;
+    }
+    
+    if(large[0] == small[0])
+        return checksequenece(large+1,small+1);
+    else
+        return checksequenece(large+1,small);
+
+}
+/*output1:
+			abchjsgsuohhdhyrikkknddg
+			coding
+			true
+            
+ output2:
+ 			abcde
+			aeb
+            false
+*/
